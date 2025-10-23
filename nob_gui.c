a0e62fc6e9024677a033c20c9e6090a7d23c3d60
Sophie <sheeperinosophie@gmail.com>
2025-10-23 11:28:38 +0200

    Update nob gui

--- nob_gui.c
+++ nob_gui.c
@@ -8,13 +8,13 @@ Cmd cmd = {0};
 
 int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
-  cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", BUILD_DIR "gui", "src/image.c", "src/color.c", "src/sorting.c", "src/gui.c", "-lm", "-lraylib");
+  cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", BUILD_DIR "gui", "src/image.c", "src/color.c", "src/sorting.c", "src/gui/gui.c", "-lm", "-lraylib");
   cmd_append(&cmd, BUILD_DIR "stb_image.o", BUILD_DIR "stb_image_write.o", BUILD_DIR "stb_image_resize.o");
   cmd_append(&cmd, "-O3", "-flto=auto", "-march=native");
-  cmd_run(&cmd);
+  if (!cmd_run(&cmd)) return 1;
 
   cmd_append(&cmd, BUILD_DIR "gui");
-  cmd_run(&cmd);
+  if (!cmd_run(&cmd)) return 1;
 
   return 0;
 }
